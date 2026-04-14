d433281356a82c220071cdf7b37eff856e4692cd
dylancondori <dcondoricorazon@gmail.com>
2026-04-14 18:45:11 -0300

    yo

--- TP3-Condori_Actividad_2/TP3-Condori_Actividad_2/src/tp3/condori_actividad_2/TP3Condori_Actividad_2.java
+++ TP3-Condori_Actividad_2/TP3-Condori_Actividad_2/src/tp3/condori_actividad_2/TP3Condori_Actividad_2.java
@@ -2,23 +2,24 @@ package tp3.condori_actividad_2;
 import java.util.Scanner;
 public class TP3Condori_Actividad_2 {
     public static void main(String[] args) {
-        Scanner na = new Scanner(System.in);
-        System.out.println("Ingrese un numero.");
-        int n = na.nextInt();
-        int contador1 = 1;
-        while(contador1 <= n){
-            System.out.println("Contador 1 = " + contador1);
-            contador1++;
+        Scanner mensaje = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num = mensaje.nextInt();
+        int c1 = 1;
+        while(c1 <= num){
+            System.out.println("Contador de while= " + c1);
+            c1++;
         }
-        System.out.println("-----------------");
-        int contador2 = 1;
+        System.out.println("___________________________");
+        int c2 = 1;
         do {            
-            System.out.println("contador 2 = " + contador2);
-            contador2++;
-        } while (contador2 <= n);
-        System.out.println("-----------------");
-        for(int c = 1; c <= n; c ++){
-            System.out.println("contador 3 = " + c);
+            System.out.println("contador de do while= " + c2);
+            c2++;
+        } 
+        while (c2 <= num);
+        System.out.println("___________________________");
+        for(int c = 1; c <= num; c ++){
+            System.out.println("contador de for = " + c);
         }
     }
     
